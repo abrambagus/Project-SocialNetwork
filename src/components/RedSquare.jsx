554d86b18769c19dbef8e7ec6e4d2a9eb632b89a
abram <abrambagus@gmail.com>
2022-11-27 17:27:41 +0700

    second commit

--- src/components/RedSquare.jsx
+++ src/components/RedSquare.jsx
@@ -5,9 +5,9 @@ const RedSquare = ({text1, text2}) => (
         <div className="flex justify-center items-center bg-red-900 w-[100%] h-[100%]">
             <div className="bg-yellow-100 p-[2px] h-[50px] w-[50px] rounded-full mr-3">
                 <div className="flex justify-center items-center bg-red-900 w-[100%] h-[100%] rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
                     stroke="currentColor" className="w-5 h-5 text-yellow-100">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
                     </svg>
                 </div>
             </div>
